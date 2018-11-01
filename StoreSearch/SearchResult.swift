7d531b016320be2e1312617d6c217266e67b3f42
Leo <leo.a.rubiano@gmail.com>
2018-11-01 17:16:18 -0400

    initial commit

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Leonardo Rubiano on 01.11.18.
+//  Copyright © 2018 leofofeo. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+    
+}
