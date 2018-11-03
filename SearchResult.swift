254c55937e925317dff5a173e97da5adece6571d
Leo <leo.a.rubiano@gmail.com>
2018-11-03 14:09:52 -0400

    Parse JSON data from iTunes and store results in searchResult array, displaying appropriate information for songs in app

---
+++ SearchResult.swift
@@ -0,0 +1,9 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Leonardo Rubiano on 11/3/18.
+//  Copyright © 2018 leofofeo. All rights reserved.
+//
+
+import Foundation
--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -8,8 +8,37 @@
 
 import Foundation
 
-class SearchResult {
-    var name = ""
-    var artistName = ""
+class ResultArray: Codable {
+    var resultCount = 0
+    var results = [SearchResult]()
+}
+
+class SearchResult: Codable, CustomStringConvertible {
+    var artistName: String? = ""
+    var trackName: String? = ""
+    var kind: String? = ""
+    var trackPrice: Double? = 0.0
+    var currency = ""
+    var imageSmall = ""
+    var imageLarge = ""
+    var storeURL: String? = ""
+    var genre = ""
     
+    enum CodingKeys: String, CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case storeURL = "trackViewUrl"
+        case genre = "primaryGenreName"
+        case kind, artistName, trackName
+        case trackPrice, currency
+    }
+    
+    
+    var name: String {
+        return trackName ?? ""
+    }
+    
+    var description: String {
+        return "Kind: \(kind ?? "None"), Name: \(name), Arist Name: \(artistName ?? "None"), Price: \(trackPrice), Artwork: \(imageSmall ?? "No image"))"
+    }
 }
