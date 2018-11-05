90051901f7704f480b10495f3191a1364a8c4e2e
Leo <leo.a.rubiano@gmail.com>
2018-11-05 15:11:36 -0500

    Display artwork for search results that have it

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SearchResultCell: UITableViewCell {
 
+    var downloadTask: URLSessionDownloadTask?
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
@@ -26,6 +28,28 @@ class SearchResultCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        downloadTask?.cancel()
+        downloadTask = nil
+    }
+    
+    // MARK:- Helper methods
+    func configure(for result: SearchResult) {
+        nameLabel.text = result.name
+        
+        if result.artist.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", result.artist, result.type)
+        }
+        
+        artworkImageView.image = UIImage(named: "Placeholder")
+        if let smallURL = URL(string: result.imageSmall) {
+            downloadTask = artworkImageView.loadImage(url: smallURL)
+        }
     }
 
 }
